36c9a07308d3949b91d0cecc3fe145dfdfc0838a
patelkhush744-cmd <patelkhush744@gmail.com>
2026-08-01 14:53:03 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Portfolio Loaded Successfully");
+window.addEventListener("scroll",()=>{
+
+const nav=document.querySelector(".navbar");
+
+if(window.scrollY>50){
+
+nav.style.background="rgba(0,0,0,.55)";
+nav.style.backdropFilter="blur(25px)";
+
+}
+else{
+
+nav.style.background="rgba(255,255,255,.08)";
+
+}
+
+});
